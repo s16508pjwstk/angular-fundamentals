0360a7d14c3887257307f61c44e58bde8d822e68
kbernatjanuszkiewicz <kacper.bernat-januszkiewicz@algolytics.pl>
2020-07-17 13:00:36 +0200

    guarding against route activation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Error404Component} from './errors/404.component';
 
 import { EventsAppComponent } from './events-app.component';
+import {EventRouteActivator} from './events/event-details/event-route-activator.service';
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavbarComponent } from "./nav/navbar.component";
@@ -19,13 +21,14 @@ import {CreateEventComponent} from "./events/create-event.component";
     EventThumbnailComponent,
     EventDetailsComponent,
     NavbarComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [EventService, ToastrService],
+  providers: [EventService, ToastrService, EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
